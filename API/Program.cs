600c9f4948f50f9b1b5acfb10282eee08296046c
LAPTOP-NV6NI1SN\Sam <willy405.14@gmail.com>
2025-12-22 11:10:12 +0800

    第三部分 - 除了 http 轉 https

--- API/Program.cs
+++ API/Program.cs
@@ -17,11 +17,18 @@ builder.Services.AddDbContext<AddDbContext>(
         );
     }
 );
+builder.Services.AddCors();    // * 配置 CORS 服務
 
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+app.UseCors(x =>        // * 使用 CORS 中介軟體，並設定允許的來源、標頭和方法
+    x.AllowAnyHeader()
+     .AllowAnyMethod()
+     .AllowAnyOrigin()
+     .WithOrigins("http://localhost:4200", "https://localhost:4200")
+);
 app.MapControllers();
 
 app.Run();
---
+++ client/src/app/app.ts
@@ -0,0 +1,38 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, inject, OnInit, signal } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { lastValueFrom } from 'rxjs';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet],
+  templateUrl: './app.html',
+  styleUrl: './app.css'
+})
+export class App implements OnInit {
+  protected readonly title = signal('Dating APP');
+  private http = inject(HttpClient);
+  protected members = signal<any>([]);
+
+  // 在組件生命週期的初始化階段發出 HTTP GET 請求
+  async ngOnInit(){
+    // 新寫法
+    this.members.set( await this.fetchMembers() );
+    
+    // 舊寫法
+    // this.http.get('https://localhost:7287/api/members').subscribe({
+    //   next: response => this.members.set(response),        // 處理成功的回應
+    //   error: error => console.log(error),               // 處理錯誤情況
+      // complete: () => console.log('Request completed')  // 請求完成後的操作
+    }
+  
+    async fetchMembers() {
+    try {
+      return lastValueFrom( this.http.get('https://localhost:7287/api/members'));
+    } catch (error) {
+      console.log(error);
+      throw error;
+    }
+  }
+
+}
